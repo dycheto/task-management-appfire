5264e4b69a7de72c99bd030a4621c9b732f2a93b
Tihomir Dukov <106734442+Dycheto@users.noreply.github.com>
2023-06-11 21:02:49 +0300

    Modal component added

---
+++ src/components/Modal.js
@@ -0,0 +1,51 @@
+export default function Modal({
+    showModal,
+    editMode,
+    setNewTask,
+    handleModalClose,
+    handleModalSubmit,
+    newTask
+}) {
+
+    
+
+    return (
+        <>
+            {showModal && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <h2>{editMode ? 'Edit Task' : 'Add Task'}</h2>
+                        <label>Title</label>
+                        <input
+                            type="text"
+                            value={newTask.title}
+                            onChange={(e) => setNewTask({ ...newTask, title: e.target.value })}
+                        />
+                        <label >Description</label>
+                        <textarea
+                            value={newTask.description}
+                            onChange={(e) => setNewTask({ ...newTask, description: e.target.value })}
+                            className="description-field"
+                        ></textarea>
+                        <label>Category</label>
+                        <select
+                            value={newTask.category}
+                            onChange={(e) => setNewTask({ ...newTask, category: e.target.value })}
+                        >
+                            <option value="Daily tasks">Daily tasks</option>
+                            <option value="Weekly tasks">Weekly tasks</option>
+                        </select>
+                        <div className="modal-buttons">
+                            <button className="modal-button" onClick={handleModalClose}>
+                                Cancel
+                            </button>
+                            <button className="modal-button" onClick={handleModalSubmit}>
+                                {editMode ? 'Save' : 'Add Task'}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
