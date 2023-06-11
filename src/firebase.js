e52f7b0af7cd6648fc9f50d820bfdd417c6d32bd
Tihomir Dukov <106734442+Dycheto@users.noreply.github.com>
2023-06-11 15:08:08 +0300

    Firebase config done

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAmW8GA88-GybKhBDkc8_yjX2FiFslU2V8",
+    authDomain: "task-management-appfire.firebaseapp.com",
+    projectId: "task-management-appfire",
+    storageBucket: "task-management-appfire.appspot.com",
+    messagingSenderId: "609959965434",
+    appId: "1:609959965434:web:461efe0ee9e4d7198e8f43"
+};
+
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
