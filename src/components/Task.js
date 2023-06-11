af6ecad09eb8275594b1b800d49d8718e2ce880d
Tihomir Dukov <106734442+Dycheto@users.noreply.github.com>
2023-06-11 15:08:38 +0300

    Task component done

---
+++ src/components/Task.js
@@ -0,0 +1,36 @@
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
+import * as taskServices from '../services/taskService';
+
+export default function Task({
+    task,
+    onEditClick
+}) {
+
+    const { userData } = useAuth();
+    const { title, description, taskId, timestamp } = task;
+    const date = new Date(timestamp.seconds * 1000);
+
+
+    const onDeleteClick = () => {
+        taskServices.deleteTask(userData, taskId).then(() => {
+            window.location.reload(false);
+        })
+    }
+
+    return (
+        <div className="task">
+            <h2 className="task-title">{title}</h2>
+            <p className="task-description">{description}</p>
+            <div className="task-footer">
+                <time className="task-time-created">{date.toLocaleString()} </time>
+                <div className="task-buttons">
+                    <button className="edit-task" onClick={onEditClick}>
+                        Edit
+                    </button>
+                    <button className="delete-task" onClick={onDeleteClick}>Delete</button>
+                </div>
+            </div>
+        </div>
+    );
+};
