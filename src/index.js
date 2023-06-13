21465a7e33c88fe06e67227d05e22158b077a29d
Tihomir Dukov <106734442+Dycheto@users.noreply.github.com>
2023-06-13 20:30:36 +0300

    error boundary added

--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import ErrorBoundary from './components/ErrorBoundary';
 import { AuthProvider } from './contexts/authContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-  <AuthProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </AuthProvider>
+  <ErrorBoundary>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </ErrorBoundary>
 );
 
 // If you want to start measuring performance in your app, pass a function
