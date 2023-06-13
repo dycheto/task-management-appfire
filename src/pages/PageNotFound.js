49f2fbfd76da5a68021e915db1a55456b42eef0d
Tihomir Dukov <106734442+Dycheto@users.noreply.github.com>
2023-06-13 20:27:31 +0300

    Pagge not found component added

---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,12 @@
+export default function PageNotFount() {
+
+    return (
+        <div className="page-not-found-wrapper">
+            <div className="page-not-found">
+                <h1>404</h1>
+                <p>Page not found</p>
+            </div>
+        </div>
+    );
+};
+
