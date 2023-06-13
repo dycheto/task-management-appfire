12d062e3e6de4c66b172406e5d2eff94a84e4882
Tihomir Dukov <106734442+Dycheto@users.noreply.github.com>
2023-06-13 20:32:23 +0300

    Update app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { signOut } from "firebase/auth";
+import { auth } from './firebase';
+import { useAuth } from './hooks/useAuth';
+import Header from './components/Header';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import Main from './pages/Main/Main';
-import Logout from './components/Logout';
 import Footer from './components/Footer';
-import { Navigate } from 'react-router-dom';
-import { useAuth } from './hooks/useAuth';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
 
-  const { isAuthenticated } = useAuth();
+  const { setUserData, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+
+    setUserData(null);
+    signOut(auth);
+    navigate("/")
+  }
 
   return (
     <div className="App">
+      <Header handleLogoutClick={handleLogout} />
       <Routes>
-        <Route
-          path="/"
-          element={isAuthenticated
-            ? <Main />
-            : <Navigate to="/login" replace />
-          }
-        />
+        <Route index element={<Login />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
-        <Route path='/logout' element={<Logout />} />
+        <Route path="/main" element={<Main />} />
+        <Route path='*' element={<PageNotFound />} />
       </Routes>
       <Footer />
     </div>
