2d2348c677873159bd10543e09709490aa300018
Tihomir Dukov <106734442+Dycheto@users.noreply.github.com>
2023-06-13 20:30:57 +0300

    footer added and css update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
 export default function Footer() {
 
+    // const handleErrorBoundary = () => {
+    //     throw new Error('Simulated error');
+    // }
+
     return (
         <footer>
+            {/* <button onClick={handleErrorBoundary}>Click me</button> */}
             <div className="footer-container">
                 <p>Developed by <span>Tihomir Dukov</span></p>
             </div>
