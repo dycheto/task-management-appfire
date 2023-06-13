18165cd9550a0d6d95fcecb4f3375c09ddd58dc2
Tihomir Dukov <106734442+Dycheto@users.noreply.github.com>
2023-06-13 20:32:30 +0300

    Update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,31 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
+import { whiteLogoImgURL } from "../Data/imagesData";
 
 export default function Header({
-    handleClick
+    handleLogoutClick
 }) {
+    const navigate = useNavigate();
 
-    const { userData } = useAuth();
+    const { isAuthenticated, userData } = useAuth();
+    const handleLogoClick = () => {
+        navigate('/')
+    }
 
     return (
         <header className="header">
-            <h1 className="app-name">organizeIt</h1>
-            <div className="user-info">
-                <strong className="username">Welcome {userData.displayName}!</strong>
-                <button className="logout-btn" onClick={handleClick}>
-                    Logout
-                </button>
-            </div>
+            <img className="logo" aria-label='logo' src={whiteLogoImgURL} alt="Logo" onClick={handleLogoClick} />
+            {isAuthenticated &&
+                <div className="user-info">
+                    <strong className="username">Welcome, {userData.displayName}!</strong>
+                    <button className="logout-btn" onClick={handleLogoutClick}>
+                        Logout
+                    </button>
+                </div>
+            }
+
         </header>
     );
 };
+
